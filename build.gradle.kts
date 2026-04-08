3748ad95401863766b6d492e5e8622f5db9f1a8c
daniilcernaev <chdanilr@gmail.com>
2026-04-08 18:52:04 +0300

    update data for publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,6 @@ plugins {
     kotlin("jvm") version "1.5.31"
     `java-gradle-plugin`
     `kotlin-dsl`
-    `maven-publish`
-    signing
 }
 
 gradlePlugin {
@@ -23,57 +21,6 @@ gradlePlugin {
     }
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "io.github.solneo"
-            artifactId = "dependency-conflict-analyzer"
-            version = "${project.version}"
-
-            from(components["java"])
-
-            pom {
-                name.set("Dependency conflict analyzer")
-                description.set("Help find conflict in gradle")
-                url.set("https://github.com/Solneo/dependency-conflict-analyzer")
-
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("dchernyaev")
-                        name.set("Daniil Chernyaev")
-                        email.set("chdanilr@gmail.com")
-                    }
-                }
-                scm {
-                    connection.set("https://github.com/Solneo/dependency-conflict-analyzer.git")
-                    developerConnection.set("git@github.com:Solneo/dependency-conflict-analyzer.git")
-                    url.set("https://github.com/Solneo/dependency-conflict-analyzer")
-                }
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            url = if ("${project.version}".contains("SNAPSHOT")) {
-                uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            } else {
-                uri("https://s01.oss.sonatype.org/content/repositories/releases/")
-            }
-            credentials {
-                username = project.properties["mavenCentralUsername"].toString()
-                password = project.properties["mavenCentralPassword"].toString()
-            }
-        }
-    }
-}
-
 java {
     withSourcesJar()
     withJavadocJar()
@@ -81,10 +28,6 @@ java {
 
 kotlin {
     jvmToolchain(17)
-}
-
-signing {
-    sign(publishing.publications["maven"])
 }
 
 group = "io.github.solneo"
